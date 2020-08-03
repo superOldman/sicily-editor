d81d65002f6c04ad71f5d34e42803003876726d9
superOldman <307485721@qq.com>
2020-08-03 10:25:51 +0800

    更新配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,12 @@
 module.exports = {
+  // 此项是用来告诉eslint找当前配置文件不能往父级查找
   root: true,
+  // 解析器  https://eslint.vuejs.org/user-guide/#usage
   parser: 'vue-eslint-parser',
   parserOptions: {
     'parser': 'babel-eslint',
     'sourceType': 'module'
   },
-
   // 指定环境
   env: {
     // 浏览器全局
@@ -13,22 +14,22 @@ module.exports = {
     node: true,
     es6: true
   },
-  // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
+  // 在这里添加更多的通用规则集  https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
   extends: [
     'plugin:vue/essential',
-    'eslint:recommended'
+    'eslint:recommended' // 设置此项会开启所有eslint推荐的规则 详见 http://eslint.cn/docs/rules/
   ],
-  // required to lint *.vue files
+  // 插件 检测 .vue 文件
   plugins: [
     // 'html'
     'vue'
   ],
 
   /**
-     * "off"或者0    关闭规则关闭
-     * "warn"或者1    在打开的规则作为警告（不影响退出代码）
-     * "error"或者2    把规则作为一个错误（退出代码触发时为1）
-     */
+  * "off"或者0    关闭规则关闭
+  * "warn"或者1    在打开的规则作为警告（不影响退出代码）
+  * "error"或者2    把规则作为一个错误（退出代码触发时为1）
+  */
   rules: {
     'semi': 1,
     'quotes': [1, 'single'], // 引号类型
